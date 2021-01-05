a3642c3029a2faf3e2ad2c3f18ca82101efe6a15
mpasche <martin.pasche@uc.cl>
2021-01-05 14:20:15 -0300

    commit

--- myproject/miproyecto/boards/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from django.urls import reverse
-
-# Create your tests here.
-class HomeTests(TestCase):
-    def test_home_view_status_code(self):
-        url = reverse('home')
-        response = self.client.get(url)
